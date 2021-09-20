085e25b834d411423e5c1f5aabb0fc1f909cab7c
John George <johndouglasgeorge@gmail.com>
2021-09-19 23:58:36 -0400

    Send and Receive clients

--- RabbitMQPractice/RabbitMQPractice/Program.cs
+++ RabbitMQPractice/RabbitMQPractice/Program.cs
@@ -1,2 +1,38 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using RabbitMQ.Client;
+using System.Text;
+
+Console.WriteLine("Hello, World! Sending");
+
+var factory = new ConnectionFactory()
+{
+    HostName = "66.175.215.111",
+    Port = 5672,
+    UserName = "testuser",
+    Password = ""
+};
+
+using var connection =  factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "ExternalQueue",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+
+var message = "Hello World";
+var body = Encoding.UTF8.GetBytes(message);
+
+
+while (true)
+{
+    channel.BasicPublish(exchange: "", routingKey: "ExternalQueue", body: body, basicProperties: null);
+    Console.WriteLine($"Sent: {message}");
+
+    var input = Console.ReadLine();
+
+    if (input == "quit") break;
+}
--- RabbitMQPractice/RabbitMQReceive/Program.cs
+++ RabbitMQPractice/RabbitMQReceive/Program.cs
@@ -1,2 +1,42 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System;
+using System.Text;
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World! Receiving");
+
+var factory = new ConnectionFactory()
+{
+    HostName = "66.175.215.111",
+    Port = 5672,
+    UserName = "testuser",
+    Password = ""
+};
+
+using var connection = factory.CreateConnection();
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "ExternalQueue",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var consumer = new EventingBasicConsumer(channel);
+
+consumer.Received += (model, ea) =>
+{
+    var body = ea.Body.ToArray();
+    var message = Encoding.UTF8.GetString(body);
+
+    Console.WriteLine($"Received: {message}");
+};
+
+channel.BasicConsume(queue: "ExternalQueue",
+    autoAck: true,
+    consumer: consumer);
+
+
+Console.ReadLine();
